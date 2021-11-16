a26cb8d0071211c570efbf30592ee53ebae18581
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2021-11-16 15:04:13 -0600

    Schema updated

---
+++ MongoDBSchema.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema({
+  "product_id": Number,
+  "name": String,
+  "slogan": String,
+  "description": String,
+  "category": String,
+  "default_price": String,
+  "features": [
+      {
+          "feature": String,
+          "value": String
+      }
+  ]
+})
+
+let StyleSchema = mongoose.Schema({
+  "product_id": Number,
+  "results": [
+    {
+      "style_id": Integer,
+      "name": String,
+      "original_price": String,
+      "sale_price": String,
+      "default?": Boolean,
+      "photos": [{
+        "thumbnail_url": String,
+        "url": String
+      }],
+      "skus": {
+        "increment": {
+          "quantity": Integer,
+          "size": String
+        }
+      }
+    }
+  ]
+})
+
+const RelatedSchema = mongoose.Schema({
+  "product-id": Number
+})
