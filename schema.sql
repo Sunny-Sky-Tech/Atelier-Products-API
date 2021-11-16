d8e136843548f7104759df8007bf4eefd28eb18b
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2021-11-16 12:12:14 -0600

    Schema file created

---
+++ schema.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS products, features, styles, skus, photos, related;
+
+CREATE TABLE products (
+  id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR(50),
+  slogan VARCHAR(125),
+  description VARCHAR(500),
+  category VARCHAR(50),
+  default_price VARCHAR(10)
+);
+
+CREATE TABLE styles (
+  style_id INTEGER NOT NULL PRIMARY KEY,
+  product_id INTEGER NOT NULL,
+  name VARCHAR(100),
+  original_price VARCHAR(15),
+  sale_price VARCHAR(15),
+  default_ BOOLEAN
+);
+
+CREATE TABLE features (
+  id INTEGER NOT NULL PRIMARY KEY,
+  product_id INTEGER,
+  feature VARCHAR(80),
+  value VARCHAR(255)
+);
+
+CREATE TABLE skus (
+  id INTEGER NOT NULL PRIMARY KEY,
+  style_id INTEGER,
+  size VARCHAR(10),
+  quantity INTEGER
+);
+
+CREATE TABLE photos (
+  id INTEGER NOT NULL PRIMARY KEY,
+  style_id INTEGER,
+  url VARCHAR(600),
+  thumbnail_url VARCHAR(600)
+);
+
+CREATE TABLE related (
+  id INTEGER NOT NULL PRIMARY KEY,
+  current_product_id INTEGER,
+  related_product_id INTEGER
+);
