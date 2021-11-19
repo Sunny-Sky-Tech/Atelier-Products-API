fbf949b0611037bff57f0de84b9da6480e561181
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2021-11-19 16:44:07 -0600

    Connection made to DB

--- db/index.js
+++ db/index.js
@@ -1,1 +1,24 @@
-const { Pool } = require('pg');
+const config = require('./config.js');
+const { Pool, Client } = require('pg');
+
+//connecting to the database
+const pool = new Pool({
+  user: config.user,
+  password: config.password,
+  host: config.host,
+  port: config.port,
+  database: config.database
+})
+
+// pool.connect(
+//   (err) => {
+//     if (err) {
+//       return console.error('Error acquiring client', err)
+//     }}
+//     console.log('connected to DB');
+// )
+
+pool.query('SELECT * FROM features', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
