4ed4e9eb3eb80bb9aa371dcae312f000490533a1
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2021-11-18 19:17:00 -0600

    CSV data loaded into the database

--- schema.sql
+++ schema.sql
@@ -1,46 +1,85 @@
+-- brew services start postgresql
+-- psql postgres -U root
+-- brew services stop postgresql
+DROP DATABASE IF EXISTS product_overview;
+
+CREATE DATABASE product_overview;
+
+\c product_overview;
+
 DROP TABLE IF EXISTS products, features, styles, skus, photos, related;
 
 CREATE TABLE products (
   id INTEGER NOT NULL PRIMARY KEY,
-  name VARCHAR(50),
-  slogan VARCHAR(125),
-  description VARCHAR(500),
-  category VARCHAR(50),
-  default_price VARCHAR(10)
+  name TEXT,
+  slogan TEXT,
+  description TEXT,
+  category TEXT,
+  default_price TEXT
 );
+
+COPY products
+FROM '/Users/yachao/Desktop/Hack Reactor/Senior Phase/SDC/CSV data/product.csv'
+DELIMITER ','
+CSV HEADER;
 
 CREATE TABLE features (
   id INTEGER NOT NULL PRIMARY KEY,
   product_id INTEGER REFERENCES products(id),
-  feature VARCHAR(80),
-  value VARCHAR(255)
+  feature TEXT,
+  value TEXT
 );
+
+COPY features
+FROM '/Users/yachao/Desktop/Hack Reactor/Senior Phase/SDC/CSV data/features.csv'
+DELIMITER ','
+CSV HEADER;
 
 CREATE TABLE styles (
   id INTEGER NOT NULL PRIMARY KEY,
-  product_id INTEGER REFERENCES products(id),,
-  name VARCHAR(100),
-  original_price VARCHAR(15),
-  sale_price VARCHAR(15),
-  default BOOLEAN
+  product_id INTEGER REFERENCES products(id),
+  name TEXT,
+  original_price TEXT,
+  sale_price TEXT,
+  default_ BOOLEAN
 );
+
+COPY styles
+FROM '/Users/yachao/Desktop/Hack Reactor/Senior Phase/SDC/CSV data/styles.csv'
+DELIMITER ','
+CSV HEADER;
 
 CREATE TABLE skus (
   id INTEGER NOT NULL PRIMARY KEY,
   style_id INTEGER REFERENCES styles(id),
-  size VARCHAR(10),
+  size TEXT,
   quantity INTEGER
 );
+
+COPY skus
+FROM '/Users/yachao/Desktop/Hack Reactor/Senior Phase/SDC/CSV data/skus.csv'
+DELIMITER ','
+CSV HEADER;
 
 CREATE TABLE photos (
   id INTEGER NOT NULL PRIMARY KEY,
   style_id INTEGER REFERENCES styles(id),
-  url VARCHAR(600),
-  thumbnail_url VARCHAR(600)
+  url TEXT,
+  thumbnail_url TEXT
 );
+
+COPY photos
+FROM '/Users/yachao/Desktop/Hack Reactor/Senior Phase/SDC/CSV data/photos.csv'
+DELIMITER ','
+CSV HEADER;
 
 CREATE TABLE related (
   id INTEGER NOT NULL PRIMARY KEY,
   product_id INTEGER REFERENCES products(id),
   related_product_id INTEGER
 );
+
+COPY related
+FROM '/Users/yachao/Desktop/Hack Reactor/Senior Phase/SDC/CSV data/related.csv'
+DELIMITER ','
+CSV HEADER;
